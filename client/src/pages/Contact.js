53cd0cd99b269037d18e7bcca2c1a075da12371c
ubaid ullah <99728959+ubaidLM@users.noreply.github.com>
2024-02-26 09:09:22 -0800

    Update Contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -13,9 +13,9 @@ const Contact = () => {
           </div>
           <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
             <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-            <a className="text-indigo-500 leading-relaxed">example@email.com</a>
+            <a className="text-indigo-500 leading-relaxed">ubaidullah.uoh@gmail.com</a>
             <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-            <p className="leading-relaxed">123-456-7890</p>
+            <p className="leading-relaxed">0349-3673578</p>
           </div>
         </div>
       </div>

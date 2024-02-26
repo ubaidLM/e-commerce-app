8d5a8767f6d8c34c25630ed49ffe2d98852472ab
ubaid ullah <99728959+ubaidLM@users.noreply.github.com>
2024-02-26 08:52:30 -0800

    server added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>ubaid ullah</h1>
+    </div>
+  )
+}
+
+export default Home
+
+// <div class="mapouter"><div class="gmap_canvas"><iframe width="820" height="560" id="gmap_canvas" src="https://maps.google.com/maps?q=totalai&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://online-timer.me/"></a><br><a href="https://www.clock-alarm.com/"></a><br><style>.mapouter{position: relative;text-align: right;height: 560px;width: 820px;}</style><a href="https://www.ongooglemaps.com">location map</a><style>.gmap_canvas{overflow: hidden;background: none !important;height: 560px;width: 820px;}</style></div></div>

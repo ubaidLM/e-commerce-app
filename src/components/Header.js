0b76319530419a70e40077af4379c13e9fd52b48
ubaid ullah <99728959+ubaidLM@users.noreply.github.com>
2024-02-26 08:46:15 -0800

    header and footer added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,66 @@
 import { UserButton } from '@clerk/clerk-react';
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
-        <header className='flex bg-[#333] h-18 justify-between'>
-            <nav className='flex justify-center items-center'>
-                <ul className='flex space-x-4'>
-                    <li className='ml-3 text-white'>
-                        <NavLink to='/' activeClassName='text-blue-500'>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li className='text-white'>
-                        <NavLink to='/About' activeClassName='text-blue-500'>
-                            About
-                        </NavLink>
-                    </li>
-                </ul>
+        <header>
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <a href="/" className="flex items-center">
+                        <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">E-Commerce</span>
+                    </a>
+                    <div className="flex items-center lg:order-2">
+                        <UserButton />
+                        <button
+                            onClick={toggleMobileMenu}
+                            data-collapse-toggle="mobile-menu-2"
+                            type="button"
+                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="mobile-menu-2"
+                            aria-expanded={isMobileMenuOpen}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
+                            </svg>
+                            <svg className={`hidden w-6 h-6`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className={`hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1`} id="mobile-menu-2" style={{ display: isMobileMenuOpen ? 'flex' : 'none' }}>
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <NavLink
+                                    to="/"
+                                    className="block py-2 px-3 text-gray-700 hover:text-gray-900"
+                                    activeClassName="font-bold" // Adjust based on your styling for active links
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    to="/Contact"
+                                    className="block py-2 px-3 text-gray-700 hover:text-gray-900"
+                                    activeClassName="font-bold" // Adjust based on your styling for active links
+                                >
+                                    Contact
+                                </NavLink>
+                            </li>
+                            {/* Add similar NavLink elements for other menu items */}
+                        </ul>
+                    </div>
+                </div>
             </nav>
-            <div className='flex items-center pr-12'>
-                <UserButton className='text-white' />
-            </div>
         </header>
     );
 };
